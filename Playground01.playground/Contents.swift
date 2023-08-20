c3be71d85af5cdb413b6b7eca6b9c4150b841c64
Jesus Ramirez <j.pasarin@outlook.com>
2023-08-20 12:47:43 -0600

    Playground 01

---
+++ Playground01.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var v1 = "Esto es Swift"
+
+let c1 = 4
+
+var cadena: String = String()
+
+cadena = "Hola Mundo " + v1
+
+print("El contenido es: \(cadena)")
+
+print("El valor constante es: \(c1)")
